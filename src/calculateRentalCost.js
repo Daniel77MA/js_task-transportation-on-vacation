f7d25a2b42514f8cc49a5bfa03e62607e6d10b81
Daniel <dania.szumski@gmail.com>
2025-08-26 13:32:06 +0200

    Done

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,12 +3,7 @@
  *
  * @return {number}
  */
-// 3. Prefer `const` over `let` where possible, to avoid unintentional changes.
-// 4. Prefer `if` with `return` over `if else` to simplify later conditions. 
-// 5. DON'T add `else` after `if` with `return` - the code after it won't be executed anyway.
-
 function calculateRentalCost(days) {
-  // write code here
   const dailyRate = 40;
   let pay = dailyRate * days;
   if (days >= 7) {
